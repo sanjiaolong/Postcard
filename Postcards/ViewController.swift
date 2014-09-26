692b73751430a77d3b6df86c377152e145df06fd
Ricky Morris <rik@server.rsmhome.me.uk>
2014-09-26 14:37:22 +0800

    First challenge solution

--- Postcards/ViewController.swift
+++ Postcards/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -20,10 +22,17 @@ class ViewController: UIViewController {
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // When we press the button
+        // message label actions
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         enterMessageTextField.text = ""
+        
+        // name label actions
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
         enterNameTextField.text = ""
+        
         
         //hide keyboard
         enterMessageTextField.resignFirstResponder()
